5c9f7ce0021e410b281dfb0757ff14dcd4e19f72
Punit Bathija <87932246+punitbathija@users.noreply.github.com>
2026-04-15 12:06:24 +0530

    Add hamburger menu toggle functionality

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ reveal.addEventListener("click", function populateFact() {
     reveal.innerHTML = "Tap to reveal";
   }, 5000);
 });
+
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("navul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
